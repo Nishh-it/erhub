6f9c0ac8de75a30a4f2404e58a9b58b733d8e24d
Nishh-it <nishalyasalvi@gmail.com>
2025-01-13 20:33:19 +0530

    vercel update

--- server/server.js
+++ server/server.js
@@ -29,6 +29,5 @@ mongoose.set("debug", true);
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// Export the express app for serverless functions
+module.exports = app;
